853b50530565257fddbf160b0bfde9fd5645707d
pjvanhall <pj@lombok.nl>
2026-06-10 13:50:17 +0200

    Split client routes into lazy chunks

--- Podium App/client/src/App.tsx
+++ Podium App/client/src/App.tsx
@@ -1,39 +1,50 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Header from './components/Layout/Header';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import TheatresPage from './pages/TheatresPage';
-import TheatreDetailPage from './pages/TheatreDetailPage';
-import AgendaPage from './pages/AgendaPage';
-import PerformanceDetailPage from './pages/PerformanceDetailPage';
-import ProfilePage from './pages/ProfilePage';
-import EditProfilePage from './pages/EditProfilePage';
-import FeedPage from './pages/FeedPage';
-import FriendRequestsPage from './pages/FriendRequestsPage';
-import SearchPage from './pages/SearchPage';
+import { LoadingState, Page } from './components/Page';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const SignupPage = lazy(() => import('./pages/SignupPage'));
+const TheatresPage = lazy(() => import('./pages/TheatresPage'));
+const TheatreDetailPage = lazy(() => import('./pages/TheatreDetailPage'));
+const AgendaPage = lazy(() => import('./pages/AgendaPage'));
+const PerformanceDetailPage = lazy(() => import('./pages/PerformanceDetailPage'));
+const ProfilePage = lazy(() => import('./pages/ProfilePage'));
+const EditProfilePage = lazy(() => import('./pages/EditProfilePage'));
+const FeedPage = lazy(() => import('./pages/FeedPage'));
+const FriendRequestsPage = lazy(() => import('./pages/FriendRequestsPage'));
+const SearchPage = lazy(() => import('./pages/SearchPage'));
+
+const routeFallback = (
+  <Page>
+    <LoadingState />
+  </Page>
+);
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/registreren" element={<SignupPage />} />
-          <Route path="/theaters" element={<TheatresPage />} />
-          <Route path="/theater/:id" element={<TheatreDetailPage />} />
-          <Route path="/agenda" element={<AgendaPage />} />
-          <Route path="/voorstelling/:id" element={<PerformanceDetailPage />} />
-          <Route path="/profiel/:id" element={<ProfilePage />} />
-          <Route path="/profiel/:id/bewerken" element={<EditProfilePage />} />
-          <Route path="/feed" element={<Navigate to="/vrienden" replace />} />
-          <Route path="/vrienden" element={<FeedPage />} />
-          <Route path="/vriendschapsverzoeken" element={<FriendRequestsPage />} />
-          <Route path="/zoeken" element={<SearchPage />} />
-        </Routes>
+        <Suspense fallback={routeFallback}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/registreren" element={<SignupPage />} />
+            <Route path="/theaters" element={<TheatresPage />} />
+            <Route path="/theater/:id" element={<TheatreDetailPage />} />
+            <Route path="/agenda" element={<AgendaPage />} />
+            <Route path="/voorstelling/:id" element={<PerformanceDetailPage />} />
+            <Route path="/profiel/:id" element={<ProfilePage />} />
+            <Route path="/profiel/:id/bewerken" element={<EditProfilePage />} />
+            <Route path="/feed" element={<Navigate to="/vrienden" replace />} />
+            <Route path="/vrienden" element={<FeedPage />} />
+            <Route path="/vriendschapsverzoeken" element={<FriendRequestsPage />} />
+            <Route path="/zoeken" element={<SearchPage />} />
+          </Routes>
+        </Suspense>
       </Router>
     </AuthProvider>
   );
